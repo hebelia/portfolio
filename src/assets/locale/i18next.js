88735221551fb887b9840b1f7a9588e808c1b3f6
hebelia <heberomeu@gmail.com>
2024-01-15 23:33:42 -0300

    feat: i18next implementation

---
+++ src/assets/locale/i18next.js
@@ -0,0 +1,23 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./en.json"; 
+import es from "./es.json";
+
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: translationEN,
+    },
+    es: {
+      translation: translationES,
+    },
+  },
+  fallbackLng: "en",
+  lng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
