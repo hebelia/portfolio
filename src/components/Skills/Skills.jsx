0b72b7d401e9dce02284a679d7b1e76b82366479
Hebe Lia <hebeliaromeu@gmail.com>
2024-01-22 01:43:50 -0300

    feat: translation skills

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "~devicon/devicon.min.css";
+
+const Skills = () => {
+  return (
+    <div>
+      <div>
+        <i className="devicon-react-original"></i>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
